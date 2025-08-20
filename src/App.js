78fbe142c719041bc5fa0ded887df5d8c53a8188
Iamdewale <dewalefams@gmail.com>
2025-08-20 14:03:42 +0100

    without NotFound page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import RoleSelector from "./pages/RoleSelector";
 import BusinessDetails from "./pages/BusinessDetails";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/404.jsx";
 
 import BusinessListingRoutes from "./routes/BusinessListingRoutes";
 import ContributorsRoutes from "./routes/ContributorsRoutes.jsx";
@@ -44,7 +44,7 @@ export default function App() {
         <Route path="/contributor/*" element={<ContributorsRoutes />} />
 
         {/* 404 */}
-        <Route path="*" element={<NotFound />} />
+        {/* <Route path="*" element={<NotFound/>} /> */}
       </Routes>
     </Router>
   );
